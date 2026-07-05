ff342cb22ceb6495a504d16b92364588249ef988
tyran721 <104623430+tyran721@users.noreply.github.com>
2026-07-05 20:47:23 +0800

    Add files via upload

---
+++ try-connect-v2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COOKIEFILE=`mktemp`
+online=$(curl -sS -L -c $COOKIEFILE "https://gw.buaa.edu.cn/cgi-bin/rad_user_info")
+rm $COOKIEFILE
+
+grep "not_online_error" <<< $online > /dev/null
+if [[ "$?" != "0" ]]; then
+        echo online: $(cut -d, -f1 <<< $online)
+        exit 1
+fi
+
+./login-v2.sh login $1
+
